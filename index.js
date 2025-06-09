cf73b5aa5348106b9d009e88a6cd3b38f7daed58
OSCAR RAUL GUALDRON VILLAMIZAR <97969534+OscarRGualdronV@users.noreply.github.com>
2025-06-08 19:38:10 -0500

    cambio archivo index

--- index.js
+++ index.js
@@ -34,3 +34,5 @@ console.log(laCajaDePandora(15));
 /* Cada integrante en su rama debe modificar la función del punto número seis (6) para que reciba como parámetro un número entero. Si es un número par, debe convertirlo a binario y retornarlo, y si es impar, convertirlo a hexadecimal y retornarlo.
 El objetivo es que cada uno resuelva el ejercicio a su modo, para luego resolver los conflictos.
 Luego de terminar la función deben pushear los cambios. */
+
+// oscar cambio
