6c33f2431f9684bbe1639792089d39252d9f87bc
OSCAR RAUL GUALDRON VILLAMIZAR <97969534+OscarRGualdronV@users.noreply.github.com>
2025-06-08 19:06:12 -0500

    Agrega función macarena con datos personales

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ console.log(laCajaDePandora(15));
 /* Cada integrante en su rama debe modificar la función del punto número seis (6) para que reciba como parámetro un número entero. Si es un número par, debe convertirlo a binario y retornarlo, y si es impar, convertirlo a hexadecimal y retornarlo.
 El objetivo es que cada uno resuelva el ejercicio a su modo, para luego resolver los conflictos.
 Luego de terminar la función deben pushear los cambios. */
+
+function oscar() {
+  return {
+    nombre: "Oscar",
+    edad: 31,
+    nacionalidad: "Colombia"
+  };
+};
+
+console.log(oscar);
