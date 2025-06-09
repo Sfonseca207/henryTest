df551e533fd71773d85d790beb9ed7a01b8d64d7
Samuel <samuelfonseca01003@gmail.com>
2025-06-08 19:05:29 -0500

    Funciion de caja de pandora

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function laCajaDePandora(numero){
-  // proximamente escribiremos codigo aqui
+function laCajaDePandora(num){
+ 
+  if(num % 2 == 0){
+    
+
+    return num.toString(2);
+
+  }else{
+
+  
+    return num.toString(16).toUpperCase();
+
+  }
+
+  
+  
 }
+
+console.log(laCajaDePandora(10)); 
+console.log(laCajaDePandora(15)); 
