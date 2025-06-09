c2a5524e03fd725125f5b870c46b0b642ecf5418
Macarena <macarena.aliberti@hotmail.com.ar>
2025-06-08 21:00:57 -0300

    Implementa conversión binaria y hexadecimal en laCajaDePandora

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function laCajaDePandora(numero){
-  // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+  if (typeof numero !== 'number' || !Number.isInteger(numero)) {
+    return 'Por favor, ingresa un número entero';
+  }
+  
+  if (numero % 2 === 0) {
+    // Número par: convertir a binario
+    return numero.toString(2);
+  } else {
+    // Número impar: convertir a hexadecimal
+    return numero.toString(16);
+  }
 }
